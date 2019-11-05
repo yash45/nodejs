86d71a82639aee4b2c2107a3afb527293516f5bf
yash45 <lebronash@gmail.com>
2019-11-05 08:33:05 +0300

    first node js submition

---
+++ intro.js
@@ -0,0 +1,20 @@
+/*
+    INTRODUCTORY NOTES ON NODE-JS
+node js is used to run javascript outside the browser 
+node js is server side javascript
+built on chrome's v8 javascript engine by ryan dhai in 2009
+
+runtime-means it executes itself 
+
+its asynchronous-node can run independently from other programs..e.g it will get 
+stuff when asked without being blocked by anything
+was used to build apps like netflix,
+
+What can it do?
+can generates dynamic page content-(means info is display in a different order by different users)
+collects data from a flowFrom: can modify data in your database
+can manage (create delete etc) files on the server
+its event driven...it doesnt wait. making it extremely fast.
+
+Read about event driven programming 
+*/
